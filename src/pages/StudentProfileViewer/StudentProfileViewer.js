cdfe7ed6b5325665ea1c7b219fe7c52af7df9bcc
Farhan Malek <malekf2328@gmail.com>
2023-09-17 13:20:36 +1200

    completed student profile viewer page ui

--- src/pages/StudentProfileViewer/StudentProfileViewer.js
+++ src/pages/StudentProfileViewer/StudentProfileViewer.js
@@ -1,49 +1,53 @@
-import React from 'react';
-import styles from './StudentProfileViewer.module.css'; // Adjust the import path as needed
-import {Link} from "react-router-dom";
+import React from "react";
+import styles from "./StudentProfileViewer.module.css"; // Adjust the import path as needed
+import { Link } from "react-router-dom";
 
 export default function StudentProfileViewer() {
   return (
     <>
-        <div className={styles.mainContainer}>
-      <div className={styles.imageCard}>
-        <img src={process.env.PUBLIC_URL + '/images/students/RawiriFletcher.png'} alt="student" />
-        <button>Edit Profile</button>
-        <button>Change Photo</button>
-      </div>
-      <div className={styles.infoCard}>
-        <h1>Rawiri Fletcher</h1>
-        <div className={styles.infoCardDetails}>
-            <div className={styles.personalDetails}>
-                <p>School</p>
-                <p>Homai School</p>              
-            </div>
-            <div className={styles.personalDetails}>
-                <p>Teacher</p>
-                <p>Jasmina Salvador</p>              
-            </div>
-            <div className={styles.personalDetails}>
-                <p>Course</p>
-                <p>Beginner</p>              
-            </div>
-            <div className={styles.personalDetails}>
-                <p>Date of Birth</p>
-                <p>25 June 2010</p>              
-            </div>
-            <div className={styles.personalDetails}>
-                <p>Contact No</p>
-                <p>022 524 6399</p>              
-            </div>
-            <div className={styles.personalDetails}>
-                <p>Email Address</p>
-                <p>fletchy.r@gmail.com</p>              
-            </div>
+      <div className={styles.mainContainer}>
+        <div className={styles.imageCard}>
+          <img
+            src={process.env.PUBLIC_URL + "/images/students/RawiriFletcher.png"}
+            alt="student"
+          />
+          <div className={styles.imageButtons}>
+            <button>Edit Profile</button>
+            <button>Change Photo</button>
+          </div>
         </div>
-
+        <div className={styles.buttonAndCard}>
+          <div className={styles.infoCard}>
+            <div className={styles.studentName}>
+              <p>Rawiri Fletcher</p>
+            </div>
+            <div className={styles.infoCardDetails}>
+              <div className={styles.studentDetailsLeft}>
+                <p>School</p>
+                <p>Teacher</p>
+                <p>Course</p>
+                <p>Date of Birth</p>
+                <p>Contact No</p>
+                <p>Email Address</p>
+              </div>
+              <div className={styles.studentDetailsRight}>
+                <p>Homai School</p>
+                <p>Jasmina Salvado</p>
+                <p>Beginner</p>
+                <p>25 June 2010</p>
+                <p>022 524 6399</p>
+                <p>fletchy.r@gmail.com</p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.projectButton}>
+            <Link to="/projectlibrary">
+              <button>Back to Projects</button>
+            </Link>
+          </div>
+        </div>
       </div>
-      <Link to="projectlibrary"><button>Back To Projects</button></Link>
-    </div>
+      <div className={styles.projectsButton}></div>
     </>
-    
   );
 }
