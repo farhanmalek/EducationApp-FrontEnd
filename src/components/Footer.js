a92ba65b7a81dbf0f6a507dfa2d36532c040242c
newton rowe <newtonrowe@gmail.com>
2023-09-18 11:43:42 +1200

    created footer js and css folders for the common use page footer

---
+++ src/components/Footer.js
@@ -0,0 +1,28 @@
+import react from 'react';
+import styles from './Footer.css';
+
+function Footer(){
+    return (
+        <footer className="footer">
+            <div className="footer-column">
+                {/* add in content */}
+            </div>
+            <div className="footer-column">
+                {/* add in content */}
+            </div>
+            <div className="footer-column">
+                {/* add in content */}
+            </div>
+            <div className="footer-column">
+                {/* add in content */}
+            </div>
+            <div className="footer-column">
+                {/* // add in content */}
+            </div>
+        </footer>
+    );
+}
+
+
+export default Footer;
+
