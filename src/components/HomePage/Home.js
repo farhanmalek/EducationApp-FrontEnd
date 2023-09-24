a27ba1eb912efabe81128ce669560be52847b25e
newton rowe <newtonrowe@gmail.com>
2023-09-25 10:29:23 +1300

    more info and sign in button added

--- src/components/HomePage/Home.js
+++ src/components/HomePage/Home.js
@@ -9,8 +9,20 @@ function Home(){
         <div className="hero" style={{ backgroundImage: `url(${heroImage})` }}>
           <div className="header-content">
             <header>
-              <h1>Prepare young minds for a better future</h1>
+              <h1>
+                Prepare young minds for a better{' '}
+                <span style={{ color: '#43c0f6' }}>future</span>
+              </h1>
+              <br></br>
               <p>Let us help you advance students in Digital Technologies and other learning areas with our project-based learning program.</p>
+              <div className="button-container">
+                <button className="learn-more-button">
+                  Learn More
+                </button>
+                <button className="sign-up-button">
+                  Sign Up
+                </button>
+            </div>
             </header>
           </div>
         </div>
