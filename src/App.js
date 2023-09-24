263e84858338d7cf71e37d1af166ab83e8391cc7
Farhan Malek <malekf2328@gmail.com>
2023-09-24 21:58:38 +1300

    deleted unneeded imports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 
 import { Route, Routes } from 'react-router-dom';
-import NavBar from "./pages/SharedItems/NavBar/NavBar";
 import ProjectLibrary from "./pages/ProjectLibrary/ProjectLibrary";
 import StudentProfileViewer from "./pages/StudentProfileViewer/StudentProfileViewer";
 import React from "react"
-import Footer from './components/Footer/Footer'
-import Home from "./components/Home/Home"
 
 
 function App() {
