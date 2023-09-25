fd2f32c7a852a5d5c035349995b4bb9ae6307040
newton rowe <newtonrowe@gmail.com>
2023-09-26 11:46:04 +1300

    page is working, color change to black

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import NavBar from "./pages/SharedItems/NavBar/NavBar";
 import ProjectLibrary from "./pages/ProjectLibrary/ProjectLibrary";
 import StudentProfileViewer from "./pages/StudentProfileViewer/StudentProfileViewer";
 import React from "react"
-import Footer from './pages/Footer/Footer'
+import Footer from './pages/SharedItems/Footer/Footer'
 import Home from "./pages/HomePage/Home"
 
 
@@ -12,12 +12,13 @@ import Home from "./pages/HomePage/Home"
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <Routes>
         <Route path="/projectlibrary" element = {<ProjectLibrary/>}/>
         <Route path="/studentprofile" element = {<StudentProfileViewer/>}/>
       </Routes>
 
-   
+      <Home/>
       
       <Footer>
       </Footer>
